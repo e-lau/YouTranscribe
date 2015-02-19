ed92cdf4b3ae7c2532f19eb97dc776cad85144fd
Daniel Pino <dannyp32@live.com>
2015-02-18 23:27:15 -0800

    fixed styling on request form

--- js/index.js
+++ js/index.js
@@ -14,6 +14,12 @@ $(function() {
         var imgURL = yt.getYouTubeThumbnail(yt.parseID($('#youtube-url').val()));
         $('#requests-container').append('<img src="' + imgURL + '">');
     });
+    
+    $('#request-form-close').click(function() {
+        if (!$('#request-form').hasClass('hidden')) {
+            $('#request-form').addClass('hidden');
+        }
+    });
 });
 
 function signinCallback(authResult) {
