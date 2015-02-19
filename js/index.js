7064e8f6a6ecb488d2925145b3b594a8b4ae4f97
Elaine Lau <elau02@calpoly.edu>
2015-02-19 02:39:10 -0800

    Finish OAuth

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+var userId = "";
+
 $(function() {
     $('#request-link').click(function() {
         if ($('#request-form').hasClass('hidden')) {
@@ -25,7 +27,43 @@ $(function() {
 function signinCallback(authResult) {
   if (authResult['status']['signed_in']) {
     console.log('Signed In!');
-  } else {
-    console.log('Sign-in state: ' + authResult['error']);
-  }
+    console.log(authResult);
+
+    // Make API call to validate the authentication token.
+    var requesturl = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=".concat(authResult.access_token);
+    var request = $.ajax({
+        url: requesturl,
+        dataType: 'json'
+    });
+
+    // Set user ID
+    request.done(function(msg) {
+        // Check if audience field matches client ID.
+        if (msg.audience == "424295941249-id3jo68q6rb0j0a8mi5o45ihneip3a4u.apps.googleusercontent.com") {
+            console.log("audience field ok");
+
+            userId = msg.user_id;
+            console.log("user id: " + userId);
+
+            // Remove sign in button
+            document.getElementById('signinButton').setAttribute('style', 'display: none');
+
+            // Get user's name
+            requesturl = "https://www.googleapis.com/plus/v1/people/".concat(userId, "?access_token=", authResult.access_token);
+            request = $.ajax({
+                url: requesturl,
+                dataType: 'json'
+            });
+            // Display welcome text
+            request.done(function(msg) {
+                console.log(msg);
+                $('#navbar ul').append("<li><a href='#accountpage'>" + msg.displayName + "</a></li>");
+            });
+       }
+    });
+
+    } 
+    else {
+        console.log('Sign-in state: ' + authResult['error']);
+    }
 }
