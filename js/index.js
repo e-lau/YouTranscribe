b6b9ff4ed6b62fe98daa4fe355729ab38028bd0d
Daniel Pino <dannyp32@live.com>
2015-02-19 01:02:53 -0800

    Fixed styling for grid layout of transcription requests. Organize css to share common styles between pages. Added transcribe.html page.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 $(function() {
+    
+    // Hardcoded until we store and retrieve this data with parse
+    var addRequest = function(title, thumbURL, rewardAmount) {
+        $('#requests-container').append('<a class="request" href="transcribe.html"><img class="video-thumb" src="http://img.youtube.com/vi/jYbx_PV3318/mqdefault.jpg"><div class="video-title">Zach LaVines 2015 Sprint Slam Dunk Contest Performance</div><div class="reward-amount">$8<span>REWARD FOR TRANSCRIBING</span></div></a>');
+    }
+    
     $('#request-link').click(function() {
         if ($('#request-form').hasClass('hidden')) {
             $('#request-form').removeClass('hidden');
         }
     });
-    
     
     $('#submit-request').click(function() {
         if (!$('#request-form').hasClass('hidden')) {
@@ -12,7 +17,7 @@ $(function() {
         }
         
         var imgURL = yt.getYouTubeThumbnail(yt.parseID($('#youtube-url').val()));
-        $('#requests-container').append('<img src="' + imgURL + '">');
+        addRequest();
     });
     
     $('#request-form-close').click(function() {
